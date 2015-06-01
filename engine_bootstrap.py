1b482a56a8de9595b2fb293d65d6181ec95a8519
Jakub Niedermertl <jniederm@redhat.com>
2015-06-01 16:56:05 +0200

    update to 3.6 host, storage domain conditionally created

--- engine_bootstrap.py
+++ engine_bootstrap.py
@@ -16,25 +16,25 @@ print 'Connected'
 print 'Updating data center'
 dc = api.datacenters.get(name='Default')
 dc.get_version().set_major(3)
-dc.get_version().set_minor(5)
+dc.get_version().set_minor(6)
 dc.update()
 print 'Data center updated'
 
 print 'Updating cluster'
 cluster = api.clusters.get(name='Default')
 cluster.get_version().set_major(3)
-cluster.get_version().set_minor(5)
-cluster.set_cpu(params.CPU(id='Intel Westmere Family'))
+cluster.get_version().set_minor(6)
+cluster.set_cpu(params.CPU(id='Intel Haswell Family'))
 cluster.update()
 print 'Cluster updated'
 
 print 'Creating host'
-host = api.hosts.get('Host1')
+host = api.hosts.get('Host_fedora22')
 if host is None:
     host = api.hosts.add(params.Host(
-        name='Host1',
+        name='Host_fedora22',
         cluster=api.clusters.get(name='Default'),
-        address='192.168.122.38',
+        address='192.168.122.199',
         root_password='a'))
     print 'Host created'
 else:
@@ -47,12 +47,16 @@ while not (host.get_status().state == 'up'):
     host = api.hosts.get(id=host.id)
 
 print 'Creating storage domain'
-storageDomain = api.storagedomains.add(
-    params.StorageDomain(name='Domain1', host=host, type_='data', storage=params.Storage(
-        type_='nfs', address='192.168.122.1', path="/home/jakub/exported-nfs"
-    ))
-)
-print 'Storage domain created'
+storageDomain = api.storagedomains.get('Domain1')
+if storageDomain is None:
+    storageDomain = api.storagedomains.add(
+        params.StorageDomain(name='Domain1', host=host, type_='data', storage=params.Storage(
+            type_='nfs', address='192.168.122.1', path="/home/jakub/exported-nfs"
+        ))
+    )
+    print 'Storage domain created'
+else:
+    print 'Storage domain already exists'
 
 print 'Attaching storage domain to datacenter'
 dc.storagedomains.add(storageDomain)
